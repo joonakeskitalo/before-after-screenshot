2f4c432f29b82306ea8647cc14d68983c90eb35c
Joona Keskitalo <joona.keskitalo@gmail.com>
2026-02-08 14:30:10 +0200

    feat: add option to copy full size image

--- script.js
+++ script.js
@@ -1,12 +1,44 @@
 const cardsEl = document.getElementById("cards");
-const copyAsImage = async () => {
+const content = document.querySelector(".content");
+
+let pasteLeftImage = false;
+
+const left = document.querySelector("#left");
+const leftDrop = left.querySelector(".drop");
+const leftImage = left.querySelector("img");
+
+const right = document.querySelector("#right");
+const rightDrop = right.querySelector(".drop");
+const rightImage = right.querySelector("img");
+
+const elementsToAdjustWidth = [
+  cardsEl,
+  content,
+  left,
+  right,
+  leftDrop,
+  rightDrop,
+  leftImage,
+  rightImage,
+];
+
+const setElementWidths = (arr, width) =>
+  arr.forEach((x) => {
+    x.style.width = width;
+  });
+
+const copyAsImage = async (useFullSize = false) => {
   try {
+    if (useFullSize) setElementWidths(elementsToAdjustWidth, "unset");
+
     const blob = await domtoimage.toBlob(cardsEl);
     navigator.clipboard.write([
       new ClipboardItem({
         "image/png": blob,
       }),
     ]);
+
+    if (useFullSize) setElementWidths(elementsToAdjustWidth, "100%");
   } catch (error) {
     console.error(error);
   }
@@ -56,16 +88,6 @@ const renderCards = () => {
 };
 
 renderCards();
-
-let pasteLeftImage = false;
-
-const left = document.querySelector("#left");
-const leftDrop = left.querySelector(".drop");
-const leftImage = left.querySelector("img");
-
-const right = document.querySelector("#right");
-const rightDrop = right.querySelector(".drop");
-const rightImage = right.querySelector("img");
 
 document.onpaste = function (event) {
   const items = (event.clipboardData || event.originalEvent.clipboardData)
